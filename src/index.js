14bd0e527fc443413c4413a279fdbf6b7dce03aa
j8seangel <jseangel@gmail.com>
2017-03-07 19:40:50 +0100

    First version

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
+import Siema from 'siema';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chart from './chart';
 import './index.css';
+
+// Init slider
+const slider = new Siema({
+  selector: '#aplications-slider',
+  duration: 200,
+  easing: 'ease-out',
+  perPage: 3,
+  startIndex: 0,
+  draggable: true,
+  threshold: 20,
+  loop: false,
+});
+
+document.querySelector('.js-slider-prev').addEventListener('click', () => slider.prev());
+document.querySelector('.js-slider-next').addEventListener('click', () => slider.next());
 
 ReactDOM.render(
   <Chart />,
